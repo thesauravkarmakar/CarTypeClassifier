3e88f98c63258d1d43ab231d0ceb0e1130359e95
thesauravkarmakar <thesauravkarmakar@gmail.com>
2020-11-11 18:59:04 +0530

    Updated for Heroku deployment

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from flask_cors import CORS, cross_origin
+from com_in_utils.utils import decodeImage
+from predict import CarType
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+
+# @cross_origin()
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.jpg"
+        self.classifier = CarType(self.filename)
+
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.predictioncartype()
+    return jsonify(result)
+
+clApp = ClientApp()
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
